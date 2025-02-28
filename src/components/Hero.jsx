425119ae26788b15515f33324168e4fccc022877
Your Name <ashutosh.singh51@outlook.com>
2025-02-28 13:04:41 +0530

    Trial_!!

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from '../constants'
-import  profilePic  from '../assets/kevinRushProfile.png'
+import  profilePic  from '../assets/1719028054915.jpg'
 import { motion } from "framer-motion"
 
 const container = (delay) => ({
